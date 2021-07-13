19c4d55e4820910688b176e18b7005db3c0c57b7
andrearep <andrea.repe@gmail.com>
2021-07-13 16:30:12 +0200

    creata tabella tramite  migrazione

---
+++ laravel/database/migrations/2021_07_13_134925_create_holidays_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->bigincrements('id');
+          
+            $table->string('departure', 30);
+            $table->string('arrival', 30);
+            $table->string('poster', 100)->nullable();
+            $table->float('price', 8, 2);
+            $table->string('transport', 20);
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
